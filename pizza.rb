f44d31c7b204b31cf5ca16be75b60bbfa8cf71a1
Michael Cooley <mikejames386@gmail.com>
2013-09-11 16:53:33 -0500

    move to laptop

--- pizza.rb
+++ pizza.rb
@@ -3,15 +3,19 @@ module Pizza
     attr_accessor :toppings
     def initialize(toppings = [Topping.new('cheese', vegetarian: true)])
       @toppings = toppings
-     
     end
+
+    def vegetarian?
+      !@toppings.any? {|topping| !topping.vegetarian }
+    end
+
   end
 
   class Topping
     attr_accessor :name, :vegetarian
 
     def initialize(name, vegetarian:false)
-      @name=name
+      @name=nameds
       @vegetarian=vegetarian
     end
   end
--- spec/pizza_spec.rb
+++ spec/pizza_spec.rb
@@ -22,7 +22,34 @@ describe Pizza::Pie do
       expect(pizza.toppings.first.name).to eq('cheese')
     end  
   end
-end 
+  describe '#vegetarian?' do
+    context 'all toppings are vegetarian' do
+      it 'returns true if all toppings are vegetarian' do
+        toppings = [
+          Pizza::Toppings.new('mushrooms', vegetarian:true)
+          Pizza::Toppings.new('peppers', vegetarian:true)
+        ]
+        pizza = Pizza::Pie.new(toppings)
+
+        expect(pizza.vegetarian?).to eq(true)
+      end
+    end
+    context 'at least one topping is non vegetarian' do
+      it 'returns false if at least one topping is non-vegetarian' do
+        toppings = [
+          Pizza::Toppings.new('mushrooms', vegetarian:true)
+          Pizza::Toppings.new('pepperoni')
+        ]
+        pizza = Pizza::Pie.new(toppings)
+
+        expect(pizza.vegetarian?).to eq(false)
+      end
+    end
+  end
+end
+
+  
+ 
 
 describe Pizza::Topping do
   describe '.initialize' do
@@ -31,7 +58,7 @@ describe Pizza::Topping do
 
       expect(topping.name).to eq('olives')
     end
-    
+
     it 'sets whether or not the topping is vegetarian' do
       topping = Pizza::Topping.new 'bell peppers', vegetarian: true
       expect(topping.vegetarian).to eq(true)
